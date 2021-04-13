c44961d0f7f4e355c29978bec71e3f9bb47cdb05
Sher Singh <75926414+sher88@users.noreply.github.com>
2021-04-13 12:58:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidestoshow:3,
+        slidestoscroll:1,
+        prevArrow:".prev-btn",
+        prevArrow:".next-btn",
+        
+
+    });
+});
